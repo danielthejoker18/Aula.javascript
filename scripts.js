5d1680c2d19904e28adcdb1e48f7c9a85eb80c86
danielthejoker18 <danielthejoker18@gmail.com>
2021-05-30 20:01:28 -0300

    Aula de js
    funções para alteração de elementos html dinamicamente

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar"
+};
+
+function trocar(elemento){
+    elemento.innerHTML = "Obrigado por passar o mouse"
+};
+function voltar(elemento){
+    elemento.innerHTML = "Passe o mouse aqui"
+};
+function change(elemento){
+    console.log(elemento.value)
+};
